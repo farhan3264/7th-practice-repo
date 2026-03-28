9df2347193cd5045523ef8c440991135d8846164
farhan3264 <farhangamer3264@gmail.com>
2026-03-29 04:13:57 +0500

    new line added

--- 7thpractice.cpp
+++ 7thpractice.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World" << std::endl;
+    std::cout << "I am Farhan" << std::endl;
     return 0;
 }
